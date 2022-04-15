1717c6cb5f2a94ae2489d219ae30621f4208f643
Miguel Bolaños <mbolanoss@unal.edu.co>
2022-04-15 10:05:57 -0500

    Added env variables to connect to the local DB
    Connection to the deployed DB is pending

--- src/go.mod
+++ src/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.31.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/kamva/mgm/v3 v3.4.1 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- src/main.go
+++ src/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/kamva/mgm/v3"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
@@ -11,7 +13,19 @@ import (
 )
 
 func initDB(){
-	err := mgm.SetDefaultConfig(nil, "profiles_db",options.Client().ApplyURI("mongodb://localhost:27017"));
+	// Loading env variables
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	DB_NAME := os.Getenv("DB_NAME")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_PORT := os.Getenv("DB_PORT")
+
+	dbURL := fmt.Sprintf("mongodb://%s:%s", DB_HOST, DB_PORT)
+	err = mgm.SetDefaultConfig(nil, DB_NAME,options.Client().ApplyURI(dbURL));
+
 	if err != nil{
 		panic("Error connecting to the database");
 	}else{
