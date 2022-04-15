a00d45c44847ce4d7b9a20101cbcdf6efc00ef42
Miguel Bolaños <mbolanoss@unal.edu.co>
2022-04-14 22:24:26 -0500

    Added date filter to played artists top

--- src/handlers/played_artists_handler.go
+++ src/handlers/played_artists_handler.go
@@ -5,6 +5,7 @@ import (
 	"profile-ms/dtos"
 	"profile-ms/helpers"
 	"profile-ms/models"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/kamva/mgm/v3"
@@ -20,41 +21,53 @@ func GetTopPlayedArtists(ctx *fiber.Ctx) error {
 	
 	var err error
 
-	topPlayedSongsDto := new(dtos.TopPlayedDto)
-	err = ctx.BodyParser(topPlayedSongsDto)
+	topPlayedDto := new(dtos.TopPlayedDto)
+	err = ctx.BodyParser(topPlayedDto)
 	if err != nil {
 		ctx.Status(http.StatusInternalServerError).SendString("Error parsing top played songs body")
 	}
 
 	var playedSongsList models.PlayedSongsList
 
-	err = mgm.Coll(&playedSongsList).First(bson.M{"username" : topPlayedSongsDto.Username}, &playedSongsList)
+	err = mgm.Coll(&playedSongsList).First(bson.M{"username" : topPlayedDto.Username}, &playedSongsList)
 
 	if err != nil {
 		return ctx.Status(http.StatusBadRequest).SendString("Username does not exist")
 	}
 	
-	playedArtists := make(map[string]int)
+	playedArtists := make(map[string]models.SongInfo)
 
-	for _, songInfo := range playedSongsList.Songs {
-		playedArtists[songInfo.ArtistName] += songInfo.Reproductions
+	//Gathering all artists reproductions according to the last update date
+	for _, artistInfo := range playedSongsList.Songs {
+		currentSongInfo := playedArtists[artistInfo.ArtistName]
+		
+		//Checking last update date
+		lastUpdate := artistInfo.LastUpdate
+		newDate :=lastUpdate.AddDate(0, 0, topPlayedDto.Gap)
+
+		//Adding reproductions
+		if(newDate.After(time.Now())){
+			currentSongInfo.Reproductions += artistInfo.Reproductions
+		}
+		
+		playedArtists[artistInfo.ArtistName] = currentSongInfo
 	}
 
 	// Sorting
-	sorted := helpers.SortMapString_Int(playedArtists)
+	artists := make(map[string]int)
+	for key, value := range playedArtists {
+		artists[key] = value.Reproductions
+	}
+	filteredAndSortedArtists := helpers.SortMapString_Int(artists)
 
 	const NUMBER_OF_ARTISTS int = 5 
 
-	if len(sorted) > NUMBER_OF_ARTISTS {
-		sorted = sorted[len(playedArtists) - NUMBER_OF_ARTISTS:]
+	if len(filteredAndSortedArtists) > NUMBER_OF_ARTISTS {
+		filteredAndSortedArtists = filteredAndSortedArtists[len(filteredAndSortedArtists) - NUMBER_OF_ARTISTS:]
 	}
 	
-	// Key: songID - Value: number of reproductions
+	// Key: Artist name - Value: number of reproductions
 	return ctx.JSON(fiber.Map{
-		"songs" : sorted,
+		"artists" : filteredAndSortedArtists,
 	})
-
-	/* for artist, times := range topPlayedArtists {
-		fmt.Println(artist, times)
-	} */
 }
